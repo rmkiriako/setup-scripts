0b8b94705fe7e6204b747578a3236b893d246c28
Rami Kiriako <rmkiriako@gmail.com>
2018-03-06 14:28:09 -0800

    Use verbose "--yes" flag instead of "-y" and ignore checksums
    - Since some applications (like Chrome) release frequently, these checksums may get out of date

--- choco-install-functions.ps1
+++ choco-install-functions.ps1
@@ -1,12 +1,12 @@
 function InstallBrowsers
 {
-    choco install firefox -y
-    choco install googlechrome -y
+    choco install firefox --yes --ignore-checksums
+    choco install googlechrome --yes --ignore-checksums
 }
 
 function InstallCdAndDvdTools
 {
-    choco install virtualclonedrive -y
+    choco install virtualclonedrive --yes --ignore-checksums
 }
 
 function InstallChocolatey
@@ -16,98 +16,98 @@ function InstallChocolatey
 
 function InstallCloudTools
 {
-    choco install cloudfoundry-cli -y
-    choco install heroku-cli -y
+    choco install cloudfoundry-cli --yes --ignore-checksums
+    choco install heroku-cli --yes --ignore-checksums
 }
 
 function InstallCommunicationTools
 {
-    choco install skype -y
-    choco install slack -y
-    choco install teamviewer -y
+    choco install skype --yes --ignore-checksums
+    choco install slack --yes --ignore-checksums
+    choco install teamviewer --yes --ignore-checksums
 }
 
 function InstallDatabaseTools
 {
-    choco install pgadmin4 -y
-    choco install robo3t -y
+    choco install pgadmin4 --yes --ignore-checksums
+    choco install robo3t --yes --ignore-checksums
 }
 
 function InstallFileSharingTools
 {
-    choco install dropbox -y
-    choco install filezilla -y
-    choco install googledrive -y  
+    choco install dropbox --yes --ignore-checksums
+    choco install filezilla --yes --ignore-checksums
+    choco install googledrive --yes --ignore-checksums
 }
 
 function InstallJavascriptTools
 {
-    choco install nodejs -y
+    choco install nodejs --yes --ignore-checksums
 }
 
 function InstallJavaAndTools
 {
-    choco install jdk8 -y
-    choco install intellijidea-ultimate -y
-    choco install ant -y
-    choco install gradle -y
-    choco install maven -y
+    choco install jdk8 --yes --ignore-checksums
+    choco install intellijidea-ultimate --yes --ignore-checksums
+    choco install ant --yes --ignore-checksums
+    choco install gradle --yes --ignore-checksums
+    choco install maven --yes --ignore-checksums
 }
 
 function InstallMarkdownEditor
 {
-    choco install markdownpad2 -y
+    choco install markdownpad2 --yes --ignore-checksums
 }
 
 function InstallMediaTools
 {
-    choco install vlc -y
+    choco install vlc --yes --ignore-checksums
 }
 
 function InstallNetworkTools
 {
-    choco install postman -y
-    choco install wireshark -y
+    choco install postman --yes --ignore-checksums
+    choco install wireshark --yes --ignore-checksums
 }
 
 function InstallPdfTools
 {
-    choco install adobereader -y
-    choco install pdfsam -y
+    choco install adobereader --yes --ignore-checksums
+    choco install pdfsam --yes --ignore-checksums
 }
 
 function InstallPythonAndTools
 {
-    choco install python3 -y
-    choco install pycharm -y
+    choco install python3 --yes --ignore-checksums
+    choco install pycharm --yes --ignore-checksums
 }
 
 function InstallSourceCodeManagementTools
 {
-    choco install git -y
-    choco install sourcetree -y
+    choco install git --yes --ignore-checksums
+    choco install sourcetree --yes --ignore-checksums
 }
 
 function InstallTerminalCommands
 {
-    choco install cygwin --params='"/InstallDir:C:\cygwin /NoStartMenu"' -y
+    choco install cygwin --params='"/InstallDir:C:\cygwin /NoStartMenu"' --yes --ignore-checksums
 }
 
 function InstallTextEditors
 {
-    choco install notepadplusplus -y
+    choco install notepadplusplus --yes --ignore-checksums
 }
 
 function InstallUtilities
 {
-    choco install 1password -y
-    choco install 7zip -y
+    choco install 1password --yes --ignore-checksums
+    choco install 7zip --yes --ignore-checksums
 }
 
 function InstallVirtualizationTools
 {
-    choco install docker -y
-    choco install virtualbox -y
+    choco install docker --yes --ignore-checksums
+    choco install virtualbox --yes --ignore-checksums
 
-    choco install vagrant -y
+    choco install vagrant --yes --ignore-checksums
 }
